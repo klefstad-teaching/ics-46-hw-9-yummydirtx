db9a92c1f0e0940133cdf891924a3d3c900aa79b
Alex Frutkin <frutkinalexander@gmail.com>
2025-03-15 18:13:17 -0700

    Refactor print_path function to simplify output formatting

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -65,10 +65,7 @@ void print_path(const vector<int>& path, int total) {
     }
     
     for (size_t i = 0; i < path.size(); ++i) {
-        cout << path[i];
-        if (i < path.size() - 1) {
-            cout << " ";
-        }
+        cout << path[i] << " ";
     }
     cout << endl << "Total cost is " << total << endl;
 }
